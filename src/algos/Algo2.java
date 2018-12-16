f9692b6a78beba564af3cc22dd1aac23e7945121
bonnet <theo.bonnet1@etu.unice.fr>
2018-12-16 17:07:39 +0100

    improve_algo_2

--- src/algos/Algo2.java
+++ src/algos/Algo2.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Algo2 extends Algos {
 
+    ArrayList<chainOfLinks> reusable = new ArrayList<>();
     public Algo2(ProblemGenerator pbg) {
         super(pbg);
     }
@@ -19,31 +20,15 @@ public class Algo2 extends Algos {
 
         for(List<Node> C : Cs) {
 
-            ArrayList<chainOfLinks> reusable = new ArrayList<>();
-
-            for (int i = 0; i < C.size() - 1; ++i) {
-                for (int j = i + 1; j < C.size(); j++) {
-                    Link l = new Link(C.get(i), C.get(j));
-                    if (links.contains(l)) {
-                        reusable.add(new chainOfLinks(l));
-                    }
+            do {
+                reusable = new ArrayList<>();
+                computeReusable(C);
+                reusable.sort(chainOfLinks::compareTo);
+                if (reusable.size() > 0) {
+                    C.removeAll(reusable.get(0).nodes);
+                    C.add(reusable.get(0).extremum[1]);
                 }
-            }
-
-            for (int i = 0; i < reusable.size() - 1; ++i){
-                for (int j = i + 1; j < reusable.size(); j++) {
-                    if(reusable.get(i).addChain(reusable.get(j))){
-                        i = 0;
-                        j = 1;
-                    }
-                }
-            }
-
-            reusable.sort(chainOfLinks::compareTo); // todo use better the chains
-            if(reusable.size()>0) {
-                C.removeAll(reusable.get(0).nodes);
-                if (C.size()>0) links.add(new Link(reusable.get(0).extremum[1],C.get(0)));
-            }
+            }while(reusable.size()>0);
             for(int i = 0; i < C.size()-1; ++i){
                 Link l = new Link(C.get(i),C.get(i+1));
                 if(!links.contains(l)){
@@ -53,5 +38,25 @@ public class Algo2 extends Algos {
         }
 
 
+    }
+
+    private void computeReusable(List<Node> C){
+            for (int i = 0; i < C.size() - 1; ++i) {
+                for (int j = i + 1; j < C.size(); j++) {
+                    Link l = new Link(C.get(i), C.get(j));
+                    if (links.contains(l)) {
+                        reusable.add(new chainOfLinks(l));
+                    }
+                }
+            }
+
+            for (int i = 0; i < reusable.size() - 1; ++i) {
+                for (int j = i + 1; j < reusable.size(); j++) {
+                    if (reusable.get(i).addChain(reusable.get(j))) {
+                        i = 0;
+                        j = 1;
+                    }
+                }
+            }
     }
 }
